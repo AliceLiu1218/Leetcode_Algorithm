ba7f0a720983cad7f67252e87fb5d621bf3b3ffb
Alice Liu <alice.liu959@gmail.com>
2026-03-27 20:52:03 -0400

    circular array queue

--- Queue_Practice.java
+++ Queue_Practice.java
@@ -4,7 +4,7 @@ public class Queue_Practice {
 	//implement queue using array
 	//this class implement stack using array, can also implement using linked list
 	//这个类以及文件夹都是为刷题准备， 数组均为整数数组, 数组长度足够大并且固定
-	//enqueue() dequeue() first() tail () size() isEmpty()
+	//enqueue() dequeue() first() tail () size() isEmpty() isFull()
 	public static class Queue {
 		public int[] queue;
 		public int l;
@@ -37,6 +37,72 @@ public class Queue_Practice {
 		}
 		public boolean isEmpty() {
 			return l == r;
+		}
+	}
+	
+	//use circular array to implement queue
+	//LC 622 test 
+	//https://leetcode.com/problems/design-circular-queue/
+	public static class Queue_CirArr {
+		public int[] queue;
+		public int l;
+		public int r;
+		public int size;
+		public int limit;
+		//constructor
+		public Queue_CirArr(int capacity) {
+			queue = new int[capacity];
+			l = 0;
+			r = 0;
+			size = 0;
+			limit = capacity;
+		}
+		
+		//methods
+		public void enqueue(int data) {
+			if(!isFull()) {
+				queue[r] = data;
+				r = r == limit-1? 0:(r+1);
+				size++;
+			}
+		}
+		//dequeue method need exception check on whether array is empty or full
+		//we use false and true ad the return value type instead of int
+		public boolean dequeue() {
+			if (isEmpty()) {
+				return false;
+			}else {
+				l = l == limit? 0: (l+1);
+				size--;
+				return true;
+			}
+			
+		}
+		//empty array. return -1
+		public int first() {
+			if (isEmpty()) {
+				return -1;
+			}else {
+				return queue[l];
+			}
+			
+		}
+		public int rear() {
+			if (isEmpty()) {
+				return -1;
+			}else {
+				int last = r==0? (limit-1): (r-1);
+				return queue[last];
+			}
+		}
+		public int size() {
+			return size;
+		}
+		public boolean isEmpty() {
+			return size == 0;
+		}
+		public boolean isFull() {
+			return size == limit;
 		}
 	}
 	
